33aec8abf26a8328c94cadc6216e61860dfb91dc
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-06 22:09:23 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -6,13 +6,13 @@ require_relative './fancy_dance.rb'
 class Kid
 
   include Dance
-   
+
     attr_accessor :name
-   
+
     def initialize(name)
       @name = name
     end
-    
+
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
